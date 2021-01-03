d6d27a98888af6ea7ce707f043d4bdba7f866485
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2021-01-03 15:41:07 +0100

    Add nav bar component

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavBar = () => {
+  return (
+    <HeaderStyle>    
+      <div>Expense</div>
+      <ul>
+        <li>Sign Up</li>
+        <li>Sign In</li>
+        <li>Expenses</li>
+        <li>Profile</li>
+      </ul>
+    </HeaderStyle>
+  )
+}
+
+export default NavBar;
+
+const HeaderStyle = styled.div`  
+  display: flex;
+  justify-content: space-between;
+    margin: 1rem 2rem;
+  div{
+    font-size: 2rem;
+    font-weight: bold;
+  }
+  ul{
+    display: flex;
+    list-style-type: none;
+    justify-content: flex-end;
+    flex-basis: 80%;
+    align-items: center;
+  }
+  li{
+    margin-left: 2rem;
+  }
+`
